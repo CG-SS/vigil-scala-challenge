a588a88628ab1bdad50997a325ca2bdd30341f6c
CGSS <criguiss@uol.com.br>
2021-10-17 18:29:58 -0300

    Adding the SecureController trait to HealthController.scala, since it should be a secure route

--- src/main/scala/dev/cgss/controllers/HealthController.scala
+++ src/main/scala/dev/cgss/controllers/HealthController.scala
@@ -3,15 +3,17 @@ package dev.cgss.controllers
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.Route
+import com.typesafe.scalalogging.LazyLogging
 import dev.cgss.models.HealthResponse
 import dev.cgss.models.HealthResponse.jsonFormat
 
-object HealthController extends Controller with SprayJsonSupport {
+object HealthController extends SecureController with SprayJsonSupport with LazyLogging {
 
   override val getEndpoints: Route =
     get {
       complete(HealthResponse())
     }
+
 
   override def pathEnd: String = "health"
 }
