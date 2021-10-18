e6be70657894c6f8d7a938e7a4a2d73587bb1b54
CGSS <criguiss@uol.com.br>
2021-10-18 02:29:49 -0300

    Using handleDbOperationComplete for handling the complete

--- src/main/scala/dev/cgss/controllers/v1/user/UserCreationController.scala
+++ src/main/scala/dev/cgss/controllers/v1/user/UserCreationController.scala
@@ -4,13 +4,14 @@ import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
 import akka.http.scaladsl.model.StatusCodes
 import akka.http.scaladsl.server.Directives.{as, complete, entity, handleExceptions, onSuccess, post}
 import akka.http.scaladsl.server.{ExceptionHandler, Route}
+import dev.cgss.controllers.v1.handleDbOperationComplete
 import dev.cgss.models.UserCreationEntity
 import dev.cgss.repository.UserRepository
 import org.postgresql.util.PSQLException
 
 object UserCreationController extends UserController with SprayJsonSupport {
 
-  val exceptionHandler: ExceptionHandler =
+  private val exceptionHandler: ExceptionHandler =
     ExceptionHandler {
       case _: PSQLException =>
         complete(StatusCodes.Conflict)
@@ -20,13 +21,8 @@ object UserCreationController extends UserController with SprayJsonSupport {
     post {
       entity(as[UserCreationEntity]) { newUser =>
         handleExceptions(exceptionHandler) {
-          val r = UserRepository.createUser(newUser.userName, newUser.email)
-          onSuccess(r) { status =>
-            if (status == 1)
-              complete(StatusCodes.Created)
-            else
-              complete(StatusCodes.Conflict)
-          }
+          val f = UserRepository.createUser(newUser.userName, newUser.email)
+          handleDbOperationComplete(f)
         }
       }
     }
