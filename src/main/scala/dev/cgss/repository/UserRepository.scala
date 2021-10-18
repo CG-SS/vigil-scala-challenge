92338e51ec498653e276a6ba4c7dcc0272d2d78e
CGSS <criguiss@uol.com.br>
2021-10-18 00:34:15 -0300

    Reformat

--- src/main/scala/dev/cgss/repository/UserRepository.scala
+++ src/main/scala/dev/cgss/repository/UserRepository.scala
@@ -4,20 +4,13 @@ import dev.cgss.models.User
 import slick.jdbc.PostgresProfile.api._
 
 import scala.concurrent.Future
+import scala.util.Random
 
 object UserRepository {
 
   private val db = Database.forConfig("postgres")
 
-  class UserTable(tag: Tag) extends Table[User](tag, None, "user") {
-    val id: Rep[Long] = column[Long]("id", O.PrimaryKey)
-    val name: Rep[String] = column[String]("name")
-    val email: Rep[String] = column[String]("email")
-
-    override def * = (id, name, email) <> (User.tupled, User.unapply)
-  }
-
-  def getUserByName(name: String) : Future[Option[User]] = {
+  def getUserByName(name: String): Future[Option[User]] = {
     val userTable = TableQuery[UserTable]
 
     val query = userTable
@@ -25,6 +18,22 @@ object UserRepository {
       .take(1)
 
     db.run(query.result.headOption)
+  }
+
+  def createUser(name: String, email: String): Future[Int] = {
+    val userTable = TableQuery[UserTable]
+
+    val insertPlayerQuery = userTable += User(Random.nextInt(), name, email)
+
+    db.run(insertPlayerQuery)
+  }
+
+  class UserTable(tag: Tag) extends Table[User](tag, None, "user") {
+    val id: Rep[Long] = column[Long]("id", O.PrimaryKey)
+    val name: Rep[String] = column[String]("name")
+    val email: Rep[String] = column[String]("email")
+
+    override def * = (id, name, email) <> (User.tupled, User.unapply)
   }
 
 }
